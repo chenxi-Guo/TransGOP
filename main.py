569cc01f1c650ecc1e789c15d51df4322f912cce
chenxi-Guo <guochenxix@qq.com>
2024-04-26 10:48:11 +0800

    rename

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_args_parser():
     # dataset parameters
     parser.add_argument('--dataset_file', default='coco')
 
-    parser.add_argument('--coco_path', type=str, default='/data1/gcx003/Datasets/gooreal/gooreal_gazedino/')
+    parser.add_argument('--coco_path', type=str, default='./Datasets/goosynth')
 
     parser.add_argument('--coco_panoptic_path', type=str)
     parser.add_argument('--remove_difficult', action='store_true')
